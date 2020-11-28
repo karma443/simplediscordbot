553c7668ec9dd92d303889c0c17f16dee605db18
karma443 <realsamuraibiz@gmail.com>
2020-11-28 19:15:05 -0300

    Removed pfp.js

--- src/commands/user/pfp.js
+++
@@ -1,9 +0,0 @@
-module.exports.run = async (args,msg,client,Discord,configFile) => {
-    
-}
-
-module.exports.config = {
-    name: "eval",
-    aliases: ["exec","run"],
-    help: {info:"Gets a user's avatar and returns ready to download.",cmd_info:"<users/roles>"}
-}
